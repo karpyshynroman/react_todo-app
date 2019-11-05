69b33dc20e1d5dd63f8c8388864e9a32558fd2bf
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-11-05 20:33:30 +0200

    Updates

--- precache-manifest.40eca10ff9164fdb8585e9a70658e762.js
+++ precache-manifest.40eca10ff9164fdb8585e9a70658e762.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846a192574418b197737554b48685981",
+    "revision": "c21e32eab327a5de5bbe77ff885c9298",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
+    "revision": "011c240c309a41df33d6",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_todo-app/static/js/2.3310f33a.chunk.js"
+    "revision": "03001256bdc820cfdad8",
+    "url": "/react_todo-app/static/js/2.146b4175.chunk.js"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
-    "url": "/react_todo-app/static/js/main.b6ea25b8.chunk.js"
+    "revision": "011c240c309a41df33d6",
+    "url": "/react_todo-app/static/js/main.744893e2.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
